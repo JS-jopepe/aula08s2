7119a3af9c9b8c4b37cb0b7458a7648ae2ae3621
JS-jopepe <jovenel.s@outlook.com>
2019-11-06 21:12:45 -0300

    TS

---
+++ aula08s22/jap/script.js
@@ -0,0 +1,9 @@
+//let player = ['Messi','Neymar','Ronaldo','Son','Dembélé'];// lista
+//let index = 0;
+//i++//incremento
+
+let player = ['Messi','Neymar','Ronaldo','Son','Dembélé'];// lista
+let indice = 0;
+for(let indice = 0; indice < player.length; indice++){
+    console.log(player[indice])
+} 
